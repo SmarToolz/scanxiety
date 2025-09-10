d17259ce092847f8c108105f147a8dcf3f8be019
Alex Rendon <alex_rendon@live.com>
2025-09-10 13:37:33 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebcamFeed from './WebcamFeed';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🧠 NeuroScan</h1>
+      <WebcamFeed />
+    </div>
+  );
+}
+
+export default App;
